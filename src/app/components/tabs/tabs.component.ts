b45cb2daf57d344755b30bee0882d35f3c45c9c0
dariolavez <daolavez@gmail.com>
2022-07-09 13:05:33 -0500

    agregado style a las tabs

--- src/app/components/tabs/tabs.component.ts
+++ src/app/components/tabs/tabs.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonTabs } from '@ionic/angular';
+import { RouteView, StackEvent } from '@ionic/angular/directives/navigation/stack-utils';
+
+
 
 @Component({
   selector: 'app-tabs',
@@ -7,8 +11,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TabsComponent implements OnInit {
 
+  @ViewChild(IonTabs) tabs:IonTabs;
+
   constructor() { }
 
   ngOnInit() {}
+
+  selectedTabHome:boolean=false;
+  selectedTabChecklist:boolean=false;
+
+
+  sss(){
+
+    let selected = this.tabs.getSelected()
+    console.log(selected)
+    if(selected=='home'){
+      this.selectedTabHome=true;
+      this.selectedTabChecklist=false
+    }
+      else if(selected=='checklist'){
+        this.selectedTabHome=false;
+        this.selectedTabChecklist=true;
+      }
+
+  }
+
+  
 
 }
